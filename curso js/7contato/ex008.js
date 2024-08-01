c2f26aa7954193823e4ebe35daaf17c1f0d91dea
Grunewald1 <116007204+Grunewald1@users.noreply.github.com>
2024-08-01 22:27:04 +0000

    js e html

---
+++ curso js/7contato/ex008.js
@@ -0,0 +1,10 @@
+var vel = 100
+console.log(`´a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log('você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log("dirija sempre usando cinto de segurança!")
+
+
+
+
---
+++ curso js/7contato/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país != 'Brasil') {
+    console.log('você é de nacionalidade Brasileiro')
+} else {
+    console.log('você é de nacionalidade Estrangeiro')
+}
